091a40fcfd221a981b0ef482be73055684098cd9
Matteo Golin <matteo.golin@gmail.com>
2022-09-28 18:01:13 -0400

    Removed unnecessary shape functions.

--- include/shapes.h
+++ include/shapes.h
@@ -7,17 +7,10 @@ typedef struct triangle {
     vector3d points[3];
 } triangle;
 
-typedef struct rawShape {
-    int vertex_count;
-    triangle *vertexes;
-} rawShape;
-
 typedef struct shape {
     int component_count;
     triangle *components;
 } shape;
 
-// Build a shape out of components
-shape *create_shape(shape *s, int shape_component_count, triangle shape_components[]);
 
 shape create_cube(float side_length);
